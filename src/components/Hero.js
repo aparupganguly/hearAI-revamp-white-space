3be4f361f22f1e961e5ba7b0f1ad7db5d5637cb6
aparupganguly <aparupganguly86@gmail.com>
2024-09-04 22:59:20 +0530

    HeroSection

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,22 @@
 import React from "react";
 import "../styles/Hero.css";
+import heroHuman from "../assets/hero/heroHuman.png";
 const Hero = () => {
   return (
-    <div className='heroContainer'>
+    <div className='heroContainer '>
       <div className='heroTextContainer'>
-        <p className='topHeroText'>Revolutionalizing Health</p>
+        <p className='F'>
+          Revolutionalizing
+          <span className='heroHealthWord'>Health</span>
+        </p>
         <p className='bottomHeroText'>
           Diagnostics with <span className='heroAiWord'>AI</span>
         </p>
       </div>
-      <p 
-      
-      className ="harnessText">harnessing biacoustic data for a healthier world</p>
+      <p className='harnessText'>
+        Harnessing biacoustic data for a healthier world
+      </p>
+      <img src={heroHuman} alt='' className='heroHuman' />
     </div>
   );
 };
