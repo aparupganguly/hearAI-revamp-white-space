8234572fa2932a61155e2d821582257771dfb961
Aviraj Paul <96339559+avirajpaul5@users.noreply.github.com>
2024-09-05 04:58:58 +0530

    Minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,17 @@ import heroBg from "../assets/hero/heroBg.png";
 import "../styles/Navbar.css";
 const Navbar = () => {
   return (
-    <div className='navbarContainer'>
-      <img src={logo} alt='' className='logo' />
-      <p className='navText'>Transforming Sound into Health Insights</p>
-      <div className='navLinks'>
+    <div className="navbarContainer">
+      <img src={logo} alt="" className="logo" />
+      <p className="navText">Transforming Sound into Health Insights</p>
+      <div className="navLinks">
         <p>Mechanism</p>
         <p>Impact</p>
-        <p>Testimoniasls</p>
+        <p>Testimonials</p>
       </div>
-      <div className='rightButtonContainer'>
-        <button className='navButton'>Discover Now</button>
-        <div className='btnLine'></div>
+      <div className="rightButtonContainer">
+        <button className="navButton">Discover Now</button>
+        <div className="btnLine"></div>
       </div>
     </div>
   );
